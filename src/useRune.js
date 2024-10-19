9a85ed86f241c6a409fb56c9cad51bfc41f93185
Arkady Elterman <elterman@gmail.com>
2024-10-18 20:46:23 -0700

    .

--- src/useRune.js
+++ src/useRune.js
@@ -1,11 +1,11 @@
 import { useAtom } from 'jotai';
 import _ from 'lodash';
 import { useCallback, useEffect, useRef, useState } from 'react';
-import { a_alert, a_app_state, a_rune_game, a_my_pid, a_my_player, a_page, a_pids, a_size, a_skill, a_turn } from './atoms';
-import { GAME_PAGE, PLAYER_JOINED, PLAYER_LEFT, START_PAGE } from './const';
-import { RA_BOARD_UPDATE, RA_INIT, RA_PERSIST, RA_PLAYER_PAGE, RA_STARTED, RA_SWITCH_PLAYER } from './logic';
+import { a_alert, a_app_state, a_my_pid, a_my_player, a_page, a_pids, a_rune_game, a_size, a_skill, a_solo, a_turn } from './atoms';
+import { GAME_PAGE, PLAYER_JOINED, PLAYER_LEFT, SIZES, SKILL_PAGE, START_PAGE } from './const';
+import { RA_BOARD_UPDATE, RA_INIT, RA_PERSIST, RA_PLAYER_PAGE, RA_SKILL_SET, RA_STARTED, RA_SWITCH_PLAYER } from './logic';
 import useGameState from './useGameState';
-import useLang, { S_NEW_GAME, S_OPP_JOINED, S_OPP_LEFT, S_PLAYER_RESTARTED } from './useLang';
+import { S_NEW_GAME, S_OPP_JOINED, S_OPP_LEFT, S_PLAYER_RESTARTED } from './useLang';
 import usePlaySound from './usePlaySound';
 import { defer, runeAction } from './utils';
 
@@ -14,18 +14,17 @@ const useRune = () => {
     const [myPlayer, setMyPlayer] = useAtom(a_my_player);
     const [pids, setPlayerIds] = useAtom(a_pids);
     const [myPlayerId] = useAtom(a_my_pid);
-    const [rg, setRuneGameState] = useAtom(a_rune_game);
-    const [runeChange, setRuneChange] = useState(null);
+    const [, setRuneGameState] = useAtom(a_rune_game);
     const [next, setNext] = useState(false);
     const [backToStart, setBackToStart] = useState(false);
-    const [, setSize] = useAtom(a_size);
-    const [, setSkill] = useAtom(a_skill);
+    const [size, setSize] = useAtom(a_size);
+    const [skill, setSkill] = useAtom(a_skill);
     const [, setTurn] = useAtom(a_turn);
     const [page, setPage] = useAtom(a_page);
+    const [solo] = useAtom(a_solo);
     const [, setAlert] = useAtom(a_alert);
-    const { getState, getStateKey, setState } = useGameState();
     const playSound = usePlaySound();
-    const { str, lang } = useLang();
+    const { getState, setState } = useGameState();
 
     const l = useRef({ queue: [], delay: 0 }).current;
     l.page = page;
@@ -34,68 +33,57 @@ const useRune = () => {
         if (backToStart) {
             setBackToStart(false);
             setPage(START_PAGE);
+            setAlert({ alert: null });
+            setSkill(0);
+            setSize(1);
 
             if (backToStart === 'init') {
-                setSize(0);
-                setSkill(0);
                 setAppState({ sounds: appState.sounds, states: {} });
             } else {
-                const states = appState.states;
-                setAppState({ states: { '00': states['00'], '01': states['01'] } });
+                SIZES.forEach((_, i) => setState(0, i + 1, {}));
             }
         }
-    }, [appState.sounds, appState.states, backToStart, setAlert, setAppState, setPage, setSize, setSkill]);
-
-    const onOppEvent = useCallback(() => {
-        const re = runeChange?.name;
-        const msg = re === PLAYER_JOINED ? S_OPP_JOINED : re === PLAYER_LEFT ? S_OPP_LEFT : null;
-
-        if (!msg) {
-            return false;
-        }
-
-        setAlert({ alert: msg });
-
-        if (page !== START_PAGE) {
-            if (myPlayerId) {
-                defer(() => runeAction(RA_INIT));
-            }
-
-            defer(() => setBackToStart(true), 2000);
-        }
-
-        return true;
-    }, [runeChange?.name, myPlayerId, page, setAlert]);
-
-    const getBoard = useCallback((gob) => {
-        const { size, skill } = gob.board_update;
-        const board = gob.board_update.boards[getStateKey(size, skill)];
-
-        return board;
-    }, [getStateKey]);
+    }, [appState.sounds, backToStart, setAlert, setAppState, setPage, setSize, setSkill, setState, size, skill]);
 
     const onBoardUpdate = useCallback((gob) => {
         const board = getBoard(gob);
-        const { size, skill } = gob.board_update;
-        const sob = getState(size, skill);
-        const { turn, scores } = board;
 
-        setState(size, skill, { ...sob, turn, scores });
-        setSize(size);
-        setSkill(skill);
+        if (!board) {
+            return;
+        };
+
+        const doUpdate = () => {
+            const { skill, size } = gob.board_update;
+            const sob = getState(skill, size);
+            const { turn } = board;
+
+            setState(skill, size, { ...sob, turn });
+            setSize(size);
+            setSkill(skill);
+        };
+
+        doUpdate();
 
         if (board.force_event === S_NEW_GAME || board.force_event === S_PLAYER_RESTARTED) {
             setAlert({ alert: board.force_event });
         }
-    }, [getBoard, getState, setAlert, setSize, setSkill, setState]);
+    }, [getState, setAlert, setSize, setSkill, setState]);
 
-    const onSwitchPlayer = useCallback((gob) => {
+    const onPlayerPage = useCallback((gob) => {
+        // TODO?
+    }, []);
+
+    const onSkillSet = useCallback((gob) => {
+        setSkill(gob.skill);
+    }, [setSkill]);
+
+    const onSwitchPlayer = useCallback(() => {
         if (page === GAME_PAGE) {
             playSound('oops', { rate: 2 });
         }
 
-        setTurn(gob.switchPlayer);
-    }, [page, playSound, setTurn]);
+        setTurn(myPlayer);
+    }, [myPlayer, page, playSound, setTurn]);
 
     const doSetNext = useCallback(() => {
         if (l.delay) {
@@ -125,37 +113,59 @@ const useRune = () => {
             case RA_BOARD_UPDATE:
                 onBoardUpdate(gob);
                 break;
+            case RA_PLAYER_PAGE:
+                onPlayerPage(gob);
+                break;
+            case RA_SKILL_SET:
+                onSkillSet(gob);
+                break;
+            case RA_SWITCH_PLAYER:
+                onSwitchPlayer();
+                break;
             default: break;
         }
 
         if (l.queue.length) {
             doSetNext();
         }
-    }, [doSetNext, l, next, onBoardUpdate]);
+    }, [doSetNext, l, next, onBoardUpdate, onPlayerPage, onSkillSet, onSwitchPlayer]);
 
-    // process onChange •••••••••••••••••••••••••••••••••••••••••••••••••••••••
-    useEffect(() => {
-        if (!runeChange) {
+    const onOppEvent = (ae) => {
+        if (l.page === START_PAGE) {
+            return false;
+        }
+
+        const re = ae?.name;
+        const msg = re === PLAYER_JOINED ? S_OPP_JOINED : re === PLAYER_LEFT ? S_OPP_LEFT : null;
+
+        if (!msg) {
+            return false;
+        }
+
+        setAlert({ alert: msg, duration: 0 });
+
+        if (myPlayerId) {
+            defer(() => runeAction(RA_INIT));
+        }
+
+        defer(() => setBackToStart(true), 2000);
+        return true;
+    };
+
+    const processOnChange = (ae, rg) => {
+        if (onOppEvent(ae)) {
             return;
         }
 
-        setRuneChange(null);
-
-        if (onOppEvent()) {
-            return;
-        }
-
-        const action = runeChange;
-        const ra = action?.name;
-        const gob = _.cloneDeep(rg);
+        const ra = ae?.name;
 
         if (ra === RA_STARTED || ra === RA_PERSIST) {
             return;
         }
 
-        if (ra === RA_BOARD_UPDATE) {
-            // console.log({ 'gob size': JSON.stringify(gob).length });
+        const gob = _.cloneDeep(rg);
 
+        if (ra === RA_BOARD_UPDATE) {
             if (l.page !== GAME_PAGE) {
                 onBoardUpdate(gob);
                 return;
@@ -166,11 +176,8 @@ const useRune = () => {
             if (!board.force_event) {
                 return;
             }
-        }
-
-        if (ra === RA_SWITCH_PLAYER) {
-            onSwitchPlayer(gob);
-            return;
+        } else if (ra === RA_SWITCH_PLAYER) {
+            onSwitchPlayer();
         }
 
         const len = l.queue.push({ ra, gob, time: Date.now() });
@@ -178,13 +185,13 @@ const useRune = () => {
         if (len === 1) {
             doSetNext();
         }
-    }, [rg, doSetNext, runeChange, getBoard, l.page, l.queue, onBoardUpdate, onOppEvent, onSwitchPlayer]);
+    };
 
     const onChange = (props) => {
         const { game, allPlayerIds, yourPlayerId, action, event } = props;
         const rg = _.cloneDeep(game);
 
-        if (!rg.started && yourPlayerId) {
+        if (!rg.started) {
             defer(() => runeAction(RA_STARTED));
         }
 
@@ -204,14 +211,10 @@ const useRune = () => {
         const player = _.findIndex(ids, id => id === yourPlayerId) + 1;
         setMyPlayer(player);
 
-        if (action?.name === RA_PLAYER_PAGE) {
-            return;
-        }
-
         const re = event?.name;
 
         if (action || re === PLAYER_JOINED || re === PLAYER_LEFT) {
-            defer(() => setRuneChange(action || event));
+            processOnChange(action || event, rg);
         } else if (re === 'stateSync' && !rg.started) {
             setBackToStart('init');
         }
@@ -228,7 +231,14 @@ const useRune = () => {
         return pi;
     };
 
-    return { onChange, playerInfo, getBoard };
+    return { onChange, playerInfo };
 };
 
 export default useRune;
+
+export const getBoard = (gob) => {
+    const { skill, size } = gob.board_update;
+    const board = gob.board_update.boards[skill][size - 1];
+
+    return _.isEmpty(board) ? null : board;
+};
